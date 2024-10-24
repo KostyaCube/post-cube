dacc99ae065c81759d21c20007a4d0b57166be53
KostyaCube <psychocube91@gmail.com>
2024-10-24 12:55:42 +0600

    added libs & mainPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import MainPage from './pages/mainPage';
 
 function App() {
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-    </>
-  );
+  return <MainPage />;
 }
 
 export default App;
---
+++ src/pages/postCard/index.tsx
@@ -0,0 +1,8 @@
+import { IProps } from '@src/types';
+import './styles.scss';
+
+function Card({ post }: IProps) {
+  return <div className="card">Post</div>;
+}
+
+export default Card;
